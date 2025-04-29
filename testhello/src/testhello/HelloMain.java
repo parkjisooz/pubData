80d9e7243b0df858021ee63c04b21b3d37d7d983
park ji soo <kkkk5425@hanmail.net>
2025-04-29 17:27:32 +0900

    [project Create] 북마켓

---
+++ testhello/src/testhello/HelloMain.java
@@ -0,0 +1,12 @@
+package testhello;
+
+public class HelloMain {
+
+	public static void main(String[] args) {
+		System.out.println("처음으로 이크립스를 통해서 깃허브에 올립니다.");
+		System.out.println("The end");
+		
+
+	}
+
+}
